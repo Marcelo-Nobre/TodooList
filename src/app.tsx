8fed3bcc652c8c1d56d0e8f11e4b88281ce7113f
Marcelo-Nobre <joaomarceloviana6@gmail.com>
2024-03-04 23:32:13 -0300

    feat: adicionando criação de tarefas

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,47 @@
 import { Header } from "./components/Header/header.tsx";
 import styles from "./App.module.css";
 // import { Task } from "./components/Task/task.tsx";
-import { PlusCircle } from "phosphor-react";
 import { Task } from "./components/Task/task.tsx";
-
-// const tasks = [
-//     {
-//         id: 1,
-//     },
-// ];
+import { PlusCircle } from "phosphor-react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export function App() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTaskText, setNewTaskText] = useState("");
+
+  function handleNewTask(event: FormEvent) {
+    event.preventDefault();
+    setTasks([...tasks, newTaskText]);
+    setNewTaskText("");
+  }
+
+  function handleNewTextTask(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value);
+    // console.log((event.target.value));
+  }
+
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
+        {/* criar novas tarefas */}
         <div className={styles.createNewTaskContent}>
-          <form className={styles.createNewTaskForm}>
-            <input name="comment" placeholder="Deixe um comentário" required />
+          <form onSubmit={handleNewTask} className={styles.createNewTaskForm}>
+            <input
+              onChange={handleNewTextTask}
+              //   value={newTask}
+              name="comment"
+              placeholder="Deixe um comentário"
+              required
+            />
 
-            <footer>
-              <button type="submit">
-                Criar <PlusCircle size={20} />
-              </button>
-            </footer>
+            <button type="submit">
+              Criar <PlusCircle size={20} />
+            </button>
           </form>
         </div>
 
+        {/* contagem de tarefas */}
         <div className={styles.listTaskStrong}>
           <strong className={styles.strongTaskCreate}>
             Tarefas criadas (0)
@@ -38,11 +53,9 @@ export function App() {
 
         <div className={styles.listTask}>
           <div>
-           {/* {tasks.map(task =>{
-                return <Task key={task.id}/>
-           })} */}
-
-           <Task/>
+            {tasks.map((task) => (
+              <Task task={task} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Task/task.tsx
+++ src/components/Task/task.tsx
@@ -1,37 +1,21 @@
 import { Trash } from "phosphor-react";
 import styles from "./task.module.css";
-import { useState } from "react";
 
-// export interface TaskProps {
-//     task: {
-//         id: number;
-//     }
-// }
+interface TaskProps {
+  task: string;
+}
 
-// interface TaskProps {
-//     task: TaskType;
-//    }
-
-export function Task( ) {
-    const [taskContainer, setTaskContainer] = useState(['Fazer café']);
-    const [newTaskText, setNewTaskText] = useState('');
-
-        function handleNewTask(event: React.FormEvent) {
-            event.preventDefault();
-            setTaskContainer([...taskContainer, newTaskText]);
-            setNewTaskText('');
-        }
-    return (
-        <div className={styles.taskContent}>
-            <input onSubmit={handleNewTask} className={styles.inputCheckboxTask} type="checkbox" value="" />
-            <p>
-                {taskContainer.map(task=> {
-
-                })}
-            </p>
-            <button title="Deletar comentário">
-                <Trash size={15} />
-            </button>
-        </div>
-    );
+export function Task({ task }: TaskProps) {
+  return (
+    <div className={styles.taskContent}>
+      {/* renderiza as tarefas */}
+      <div className={styles.task}>
+        <input className={styles.inputCheckboxTask} type="checkbox" value="" />
+        <p>{task}</p>
+        <button>
+          <Trash size={15} />
+        </button>
+      </div>
+    </div>
+  );
 }
